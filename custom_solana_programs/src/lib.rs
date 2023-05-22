2208f2b92e84fecad1a7bbeb8d2160b679a20595
Gleb <bigman212@yandex.ru>
2023-05-22 05:36:42 +0300

    add hello_name_program and its usage (#1)

---
+++ custom_solana_programs/src/lib.rs
@@ -0,0 +1,50 @@
+pub mod hello_name_program {
+    use std::str::FromStr;
+
+    use borsh::{BorshDeserialize, BorshSerialize};
+    use solana_program::{
+        account_info::AccountInfo,
+        entrypoint,
+        entrypoint::ProgramResult,
+        msg
+    };
+    use solana_program::pubkey::Pubkey;
+
+    pub const PROGRAM_ID: &str = "2BYjDV3FzQxLLAzyAtYq8Lzucet44bsSn8Gv2tvsz5BY";
+
+    pub fn program_id() -> Pubkey {
+        return Pubkey::from_str(PROGRAM_ID).unwrap()
+    }
+
+    #[derive(BorshSerialize, BorshDeserialize, PartialEq, Debug)]
+    pub struct NamesData {
+        pub group_leader: String,
+        pub group_members: Vec<String>,
+    }
+
+    entrypoint!(hello_name_program);
+
+    pub fn hello_name_program(
+        program_id: &Pubkey,
+        accounts: &[AccountInfo],
+        instruction_data: &[u8],
+    ) -> ProgramResult {
+        let decoded_data =
+            NamesData::try_from_slice(&instruction_data)
+                .unwrap();
+
+        // log a message to the blockchain
+        msg!("Hello, {}!", decoded_data.group_leader);
+        decoded_data.group_members
+            .iter()
+            .for_each(|member_name| {
+                msg!("Hello member, {}, welcome.", member_name)
+            });
+
+        // gracefully exit the program
+        Ok(())
+    }
+}
+
+
+
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,32 @@
-mod create_account;
+use std::env;
 
+use custom_solana_programs::hello_name_program;
 use solana_client::rpc_client::RpcClient;
 use solana_sdk::account::Account;
-use solana_sdk::native_token::{lamports_to_sol};
+use solana_sdk::native_token::lamports_to_sol;
 use solana_sdk::signature::{Keypair, read_keypair_file};
 use solana_sdk::signer::Signer;
-use crate::create_account::create_account_utils;
+
+use crate::transactions::create_account::custom_transactions::call_hello_name;
+
+mod transactions;
 
 fn main() {
+    let root_wallet_path: Vec<String> = env::args().collect();
+    let instruction_data = hello_name_program::NamesData {
+        group_leader: String::from("KeyApp"),
+        group_members: vec!["Chingiz", "Nick", "Tengiz", "Kostya", "Davran", "Eduard"]
+            .iter()
+            .map(|&x| String::from(x))
+            .collect(),
+    };
     let rpc_client = RpcClient::new("http://localhost:8899");
-    let root_account = load_wallet_from_local(&rpc_client);
-    create_account_utils::create_new_account(&rpc_client, &root_account.keypair)
-        .expect("Expected ok");
+    let root_account = load_wallet_from_local(&rpc_client, &root_wallet_path[1]);
+
+    call_hello_name(&instruction_data, &root_account.keypair, rpc_client);
+
+    // custom_transactions::create_new_account(&rpc_client, &root_account.keypair)
+    //     .expect("Expected ok");
 
     return;
 }
@@ -21,8 +36,8 @@ struct RootAccount {
     account: Account,
 }
 
-fn load_wallet_from_local(rpc_client: &RpcClient) -> RootAccount {
-    let wallet = read_keypair_file(&*shellexpand::tilde("~/.config/solana/id.json"))
+fn load_wallet_from_local(rpc_client: &RpcClient, path: &str) -> RootAccount {
+    let wallet = read_keypair_file(path)
         .expect("Example requires a keypair file");
     let pub_key = wallet.pubkey();
 
